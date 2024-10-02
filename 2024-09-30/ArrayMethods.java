8075550529a34f59980d1bf6889fe685cfc093fd
Ansel Darnell <adarnell60@stuy.edu>
2024-10-01 20:54:23 -0400

    Added testcases + classwork + array copy

--- 2024-09-30/ArrayMethods.java
+++ 2024-09-30/ArrayMethods.java
@@ -75,6 +75,42 @@ public static int arr2DSum(int[][]nums){
   return sum;//place holder return value so it compiles.
 }
 
+//3. Modify a given 2D array of integer as follows:
+//Replace all the negative values:
+//-When the row number is the same as the column number replace
+//that negative with the value 1
+//-All other negatives replace with 0
+public static void replaceNegative(int[][] vals){
+  for(int i = 0; i < vals.length; i++){
+    for(int k = 0; k < vals[i].length; k++){
+      if(vals[i][k] < 0){
+         if(i == k){
+           vals[i][k] = 1;
+         }
+         else{
+           vals[i][k] = 0;
+         }
+      }
+    }
+  }
+}
+
+//4. Make a copy of the given 2d array.
+//When testing : make sure that changing the original does NOT change the copy.
+//DO NOT use any built in methods that "copy" an array.
+//You SHOULD write a helper method for this.
+//If you don't see a good way to do that, you should stop and look at prior methods.
+public static int[][] copy(int[][] nums) {
+    int[][] result = new int[nums.length][];
+    for (int i = 0; i < nums.length; i++) {
+        result[i] = new int[nums[i].length];
+        for (int k = 0; k < nums[i].length; k++) {
+            result[i][k] = nums[i][k];
+        }
+    }
+    return result;
+}
+
 /**Rotate an array by returning a new array with the rows and columns swapped.
   * You may assume the array is rectangular and neither rows nor cols is 0.
   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
@@ -121,6 +157,25 @@ public static void main (String[] args) {
   System.out.println("Should return [[11], [], [31, 32, 33, 34]] ;;;;;;;  " + "returns " + arrToString(testCase1));
   System.out.println("Should return 141" + ";;;;;;;; returns " + arr2DSum(testCase1));
 
+  testCase1 = new int[][] {{11,-12,13,-14}, {-21,-22,23,-24}, {-31,32,33,34}};
+  replaceNegative(testCase1);
+  System.out.println("Should return: [[11, 0, 13, 0], [0, 1, 23, 0], [0, 32, 33, 34] ] :::::::::: Returned: " + arrToString(testCase1));
+
+  testCase1 = new int[][] {{1,2,3,4,5}, {1,2}, {}};
+  System.out.println("Adresses should be different:");
+  System.out.println(testCase1);
+  System.out.println(copy(testCase1));
+  System.out.println("Should return [[1, 2, 3, 4, 5], [1,2] []]:::::: returns: " + arrToString(copy(testCase1)));
+  testCase1 = new int[][] {{1,-2,3,-4,5}, {}, {1,1,1}};
+  System.out.println("Adresses should be different:");
+  System.out.println(testCase1);
+  System.out.println(copy(testCase1));
+  System.out.println("Should return [[1, -2, 3, -4, 5], [] [1, 1, 1]]:::::: returns: " + arrToString(copy(testCase1)));
+  testCase1 = new int[][] {{}};
+  System.out.println("Adresses should be different:");
+  System.out.println(testCase1);
+  System.out.println(copy(testCase1));
+  System.out.println("Should return [[]]:::::: returns: " + arrToString(copy(testCase1)));
 }
 
 }
