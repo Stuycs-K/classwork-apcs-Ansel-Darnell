a7dfc5a91da7be2e50d41b1adb3d48e26f94d8b1
Ansel-Darnell <Adarnell60@stuy.edu>
2024-10-15 11:48:07 -0400

    added more test cases

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -20,5 +20,22 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+    System.out.println( distance(p1,p3));
+    System.out.println( Point.distance(p1,p3));
+    System.out.println( p1.distanceTo(p3));
+    p1 = new Point(11,17);
+    p2 = new Point(-11,-1.3);
+    p3 = new Point(31,40.2);
+    test = new Point (p1);
+    test1 = new Point (p2);
+    System.out.println(test);
+    System.out.println(test1);
+    System.out.println( p3);
+    System.out.println( distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+    System.out.println( distance(p1,p3));
+    System.out.println( Point.distance(p1,p3));
+    System.out.println( p1.distanceTo(p3));
   }
 }
