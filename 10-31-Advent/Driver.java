4b41353551b5fbf2587b95c624796c17f0b894ec
Ansel Darnell <adarnell60@stuy.edu>
2024-11-05 21:17:59 -0500

    final code after second day: code was bad EG:  isTrig changed to be much more efficient

---
+++ 10-31-Advent/Driver.java
@@ -0,0 +1,8 @@
+public class Driver
+{
+  public static void main(String[] args)
+  {
+    System.out.println(TriangleTester.countTrianglesB("inputTri.txt"));
+    System.out.println(TriangleTester.countTrianglesB("inpi.txt"));
+  }
+}
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -3,46 +3,59 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriangleTester{
-  public static void main(String[] args){
-    System.out.println(isTrig(new int[] {1,11,1312}));
-    System.out.println(isTrig(new int[] {5,11,13}));
+
+  private static boolean isTrig(int v0, int v1, int v2){
+    return ( (((v0 + v1) > v2) && ((v0 + v2) > v1)) && ((v1 + v2) > v0));
   }
 
   public static int countTrianglesA(String filename){
-    int[] sides = new int[3];
-    File fille = new File(filename);
-    Scanner file = new Scanner(fille);
-    int count = 0;
-    while(file.hasNextLine()){
-      int index = 0;
-      while(file.hasNextInt()){
-        sides[index] = file.nextInt();
-        index ++;
+    try{
+      File fille = new File(filename);
+      Scanner file = new Scanner(fille);
+      int count = 0;
+      while(file.hasNextLine())
+      {
+        int v0 = Integer.parseInt(file.next());
+        int v1 = Integer.parseInt(file.next());
+        int v2 = Integer.parseInt(file.next());
+        if (isTrig(v0, v1, v2)){
+          count++;
         }
-      if(isTrig(sides)){
-        count ++;
+        file.nextLine();
       }
-      sides = new int[3];
+      return count;
+    }
+    catch (FileNotFoundException e){
+      System.out.println("file not found");
+      return -1;
     }
   }
 
-  public static boolean isTrig (int[] sides){
-    int max = 0;
-    int second = 0;
-    int min = 0;
-    for(int i =0; i < 3; i++){
-      if(sides[i] > max){
-        second = max;
-        max = sides[i];
-      }
-      else if (sides[i] > second){
-        min = second;
-        second = sides[i];
-      }
-      else{
-        min = sides[i];
+  public static int countTrianglesB(String filename){
+    try{
+      File fille = new File(filename);
+      Scanner file = new Scanner(fille);
+      int count = 0;
+      while(file.hasNextLine()){
+        int[] t0 = new int[3];
+        int[] t1 = new int[3];
+        int[] t2 = new int[3];
+        for (int k = 0; k < 3; k++){
+            t0[k] = Integer.parseInt(file.next());
+            t1[k] = Integer.parseInt(file.next());
+            t2[k] = Integer.parseInt(file.next());
+            file.nextLine();
+        }
+        count += (isTrig(t0[0],t0[1],t0[2])) ? 1 :0;
+        count += (isTrig(t1[0],t1[1],t1[2])) ? 1 :0;
+        count += (isTrig(t2[0],t2[1],t2[2])) ? 1 :0;
+        }
+        return count;
+    }
+      catch (FileNotFoundException e){
+        System.out.println("file not found");
+        return 0;
       }
     }
-    return (sides[1] + sides[2] > sides[0]);
-  }
+
 }
