efac1685585f8a7c164dedfc7d68d45af296963f
Ansel Darnell <adarnell60@stuy.edu>
2024-11-14 19:21:28 -0500

    tested run times, pushing driver all commented out

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 public class Driver {
   public static void main(String[] args) {
-    ArrayList<String> test = ArrayListPractice.createRandomArray(20);
-    ArrayList<String> test2 = ArrayListPractice.createRandomArray(20);
-    //ArrayListPractice.replaceEmpty(test);
-    //System.out.println(test);
-    //System.out.println(ArrayListPractice.makeReversedList(test));
     /*
+    ArrayList<String> test = ArrayListPractice.createRandomArray(200000);
+    ArrayList<String> test2 = ArrayListPractice.createRandomArray(200000);
+    ArrayListPractice.replaceEmpty(test);
+    System.out.println(test);
+    System.out.println(ArrayListPractice.makeReversedList(test));
+
     System.out.println("SAME LENGTH MIX LIST");
     System.out.println(ArrayListPractice.mixLists(test, test2));
     System.out.println(test);
     System.out.println(test2);
     test = ArrayListPractice.createRandomArray(0);
-    test2 = ArrayListPractice.createRandomArray(20);
+    test2 = ArrayListPractice.createRandomArray(0);
     System.out.println("0 AND 20 LENGTHS FIRST 2 ARE THE INPUTS");
     System.out.println(test);
     System.out.println(test2);
