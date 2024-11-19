53dbb0aa3c85e76284630e54ebe102a5f7cbe9fb
Ansel Darnell <adarnell60@stuy.edu>
2024-11-18 22:56:02 -0500

    worked on parsing and direction, counting may work but trouble when reading file

--- 2024-11-18-advent2016/Code.java
+++ 2024-11-18-advent2016/Code.java
@@ -3,20 +3,39 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Code{
-  public static void distance(Filename){
-    File fille = new File(filename);
-    Scanner file = new Scanner(fille);
-    String[] text = file.nextLine().split(",")
-    int horizontalDistance;
-    int verticalDistance;
-    int[] directions = {1,1,-1,-1};
-    int pos = 0;
-    for(int i = 0; i < text.length; i ++){
-      if(text[i].charAt(0) == "R"){
-        turn("right", pos)
-      }
-    }
+  public static void main(String[] args){
+    System.out.println(distance("input.txt"));
+  }
+  public static int distance(String filename){
+    try{
+      File fille = new File(filename);
+      Scanner file = new Scanner(fille);
+      String[] text = file.nextLine().split(",");
+      int horizontalDistance=0;
+      int verticalDistance=0;
+      int[] directions = {1,1,-1,-1};
+      int pos = 0;
+      for(int i = 0; i < text.length; i ++){
+        if(text[i].charAt(0) == 'R'){
+          pos = turn("right", pos);
+        }
+        else{         //(text[i].charAt(0) == "L"){
+          pos = turn("left", pos);
+        }
+        if ((pos == 0) || (pos == 2)){
+          verticalDistance += Integer.parseInt(text[i].substring(0)) * directions[pos];
+        }
+        else{
+          horizontalDistance += Integer.parseInt(text[i].substring(0)) * directions[pos];
+        }
 
+      }
+      return horizontalDistance + verticalDistance;
+    }
+    catch (FileNotFoundException e){
+      System.out.println("file not found");
+      return -1;
+    }
 }
 
   public static int turn(String direction, int pos){
@@ -25,15 +44,17 @@ public class Code{
         pos = 0;
       }
       else{
-        pos += 1
+        pos += 1;
       }
     }
     else{
       if(pos == 0){
-        pos == 3;
+        pos = 3;
       }
       else{
         pos -= 1;
       }
     }
+    return pos;
   }
+}
