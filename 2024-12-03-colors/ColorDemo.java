beeb6a32b6770538bdf06f6e692d56fe14897b8f
Ansel-Darnell <Adarnell60@stuy.edu>
2024-12-03 11:59:26 -0500

    start of color project

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,17 @@
+public class ColorDemo{
+  public static void main(String[] args)
+    System.out.print(as);
+    System.out.println("Hello");
+    System.out.print("\u001b[0m");
+    System.out.println("bye");
+  }
+  public static final String as = "\u001b[36m";
+  public static final String clear = "\u001b[2J";
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+
+  public void draw(int times){ //randomly draws a polluck some number of times//
+    for(int i = 0; i < )
+  }
+}
