2788d84e50feb9575ee057b10826278dd565fa7a
Ansel Darnell <adarnell60@stuy.edu>
2024-12-03 20:48:57 -0500

    finished draw - it prints blocks by total blocks and blocks per column

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,17 +1,19 @@
-public class ColorDemo{
-  public static void main(String[] args)
-    System.out.print(as);
-    System.out.println("Hello");
-    System.out.print("\u001b[0m");
-    System.out.println("bye");
-  }
-  public static final String as = "\u001b[36m";
-  public static final String clear = "\u001b[2J";
-  public static final String CLEAR_SCREEN =  "\u001b[2J";
-  public static final String HIDE_CURSOR =  "\u001b[?25l";
-  public static final String SHOW_CURSOR =  "\u001b[?25h";
+public class ColorDemo {
+    public static void main(String[] args) {
+        draw((200 * 50), 200); // Draw 100 blocks, with 50 blocks per row
+    }
 
-  public void draw(int times){ //randomly draws a polluck some number of times//
-    for(int i = 0; i < )
-  }
+    public static void draw(int times, int col) { // Randomly draws a "polluck" of n blocks
+        String block = "█";
+        for (int i = 0; i < times; i++) {
+            String thing = "\u001b[";
+            int colorCode = (int) (Math.random() * 8) + 30;
+            System.out.print(thing + colorCode + "m");
+            System.out.print(block);
+            if ((i + 1) % col == 0) {
+                System.out.println();
+            }
+        }
+        System.out.print("\u001b[0m");
+    }
 }
