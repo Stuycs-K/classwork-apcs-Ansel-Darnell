0a474fe85c4dc249a4830fcbac708aeb439593cc
Ansel Darnell <adarnell60@stuy.edu>
2024-12-08 23:06:48 -0500

    testcases in driver

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -4,7 +4,12 @@ public class Driver{
     Maker p1 = new Maker("Ziggy",1000000);
     Maker p2 = new Maker("Jerry", 10, "Oil", 17);
     System.out.println(p2.attack(p1));
-
+    System.out.println(p2.getSpecialName());
+    p2.setSpecial(3);
+    System.out.println(p2.getSpecial());
+    System.out.println(p2.getSpecialMax());
+    System.out.println(p2.support(p1));
+    System.out.println(p2.specialAttack(p1));
 
 
   }
