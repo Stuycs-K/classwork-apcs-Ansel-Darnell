5358451e490dcce349a95ed4d2b499ecc9a7327b
Ansel-Darnell <Adarnell60@stuy.edu>
2025-01-02 11:44:13 -0500

    finished fib work

---
+++ 2025-1-2/fibonacci.java
@@ -0,0 +1,17 @@
+public class fibonacci
+{
+  public static void main(String[] args) {
+    System.out.println(fibonacci(Integer.parseInt(args[0])));
+  }
+  public static int fibonacci(int n)
+  {
+    if (n < 2)
+    {
+      return n;
+    }
+    else
+    {
+      return( fibonacci(n - 1) + fibonacci(n - 2));
+    }
+  }
+}
